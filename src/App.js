9a2d9a4b9a443a2f3cd628c345bddf76227c30d6
Nandeep Shah <nandeep.shah@gmail.com>
2023-01-09 20:07:37 -0500

    adding posts lists

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React from 'react'
+import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
 
-import { Navbar } from './app/Navbar'
+import { Navbar } from './app/Navbar';
+import PostsList from './features/posts/PostsList';
+import AddPostForm from './features/posts/AddPostForm';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
-            )}
-          />
-          <Redirect to="/" />
-        </Switch>
-      </div>
-    </Router>
-  )
+	return (
+		<Router>
+			<Navbar />
+			<div className='App'>
+				<Switch>
+					<Route
+						exact
+						path='/'
+						render={() => (
+							<React.Fragment>
+								<AddPostForm />
+								<PostsList />
+							</React.Fragment>
+						)}
+					/>
+					<Redirect to='/' />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
-export default App
+export default App;
